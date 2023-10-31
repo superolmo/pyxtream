cc62b3ed78da97b0f38438aab8ef597ee7b9936e
Richard de Vos <richard.de.vos@sentia.com>
2023-10-31 20:50:20 +0100

    fix flask mandatory

--- pyxtream/__init__.py
+++ pyxtream/__init__.py
@@ -1,5 +1,8 @@
 
 from .pyxtream import XTream
-from .rest_api import FlaskWrap
+try:
+    from .rest_api import FlaskWrap
+except:
+    pass
 from .progress import progress
 from .version import __version__, __author__, __author_email__
