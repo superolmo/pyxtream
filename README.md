18e3818b0a60f65f6992fbe1e38b3cb0a330a411
Claudio Olmi <superolmo2@gmail.com>
2024-09-02 00:17:19 -0500

    More adjustments

--- README.md
+++ README.md
@@ -54,7 +54,11 @@ python3 functional_test.py
 
 The functional test will allow you to authenticate on startup, load and search streams. If Flask is installed, a simple website will be available at http://localhost:5000 to allow you to search and play streams.
 
-### Interesting Work by somebody else 
+## Interesting Work by somebody else 
+
+- xtreamPOC - Project is a Proof of Concept (POC) that leverages pyxtream, MPV, and NiceGUI to demonstrate the use of Xtream Portal Codes.
+
+## 
 
 So far there is no ready to use Transport Stream library for playing live stream.
 
@@ -112,6 +116,7 @@ xTream.movies[{},{},...]
 
 | Date | Version | Description |
 | ----------- | -----| ----------- |
+| 2024-09-02 | 0.7.2 | - <br>- <br>- |
 | 2024-05-21 | 0.7.1 | - Fixed missing jsonschema package<br>- Fixed provider name in functional_test<br>- Improved print out of connection attempts<br>- Added method to read latest changes in functional_test
 | 2023-11-08 | 0.7.0 | - Added Schema Validator<br>- Added Channel Age<br>- Added list of movies added in the last 30 and 7 days<br>- Updated code based on PyLint<br>- Fixed Flask package to be optional [richard-de-vos](https://github.com/richard-de-vos)|
 | 2023-02-06 | 0.6.0 | - Added methods to change connection header, to turn off reload timer, and to enable/disable Flask debug mode<br>- Added a loop when attempting to connect to the provider <br>- Cleaned up some print lines|
--- functional_test.py
+++ functional_test.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""
+Test application to validate provider account access
+"""
 
 import sys
 from time import sleep
@@ -29,9 +32,9 @@ def str2list(input_string: str) -> list:
 
     # convert each element as integers
     li = []
-    for i in output_list:
+    for list_index in output_list:
         try:
-            li.append(int(i))
+            li.append(int(list_index))
         except ValueError:
             pass
 
@@ -68,6 +71,7 @@ while True:
         (5) Download Video Impl (URL, filename)
         (6) Show how many movies added in past 30 days
         (7) Show how many movies added in past 7 days
+        (8) Get Series Info By ID (series_id)
         ----------
         (0) Quit
         """
@@ -90,24 +94,29 @@ while True:
 
         elif choice == 2:
             search_string = input("Search for REGEX (ex. '^Destiny.*$'): ")
-            search_result_obj = xt.search_stream(search_string)
+            search_result_obj = xt.search_stream(search_string, stream_type=("movies","series"))
             result_number = len(search_result_obj)
             print(f"\tFound {result_number} results")
             if result_number < 10:
                 for stream in search_result_obj:
-                    print(f"Found `{stream['name']}` at URL: {stream['url']}")
+                    if 'url' in stream:
+                        print(f"Found `{stream['name']}` at URL: {stream['url']}")
+                    else:
+                        print(stream)
 
         elif choice == 3:
             search_string = input("Search for text: ")
-            search_result_obj = xt.search_stream(r"^.*{}.*$".format(search_string))
+            search_result_obj = xt.search_stream(
+                rf"^.*{search_string}.*$", stream_type=("movies","series")
+                )
             result_number = len(search_result_obj)
             print(f"\tFound {result_number} results")
             if result_number < 10:
                 for stream in search_result_obj:
-                    try:
+                    if 'url' in stream:
                         print(f"Found {stream['name']} at URL: {stream['url']}")
-                    except KeyError:
-                        print("Exception")
+                    else:
+                        print(stream)
 
         elif choice == 4:
             stream_id = input("Stream ID: ")
@@ -127,15 +136,21 @@ while True:
             xt._download_video_impl(url,filename)
 
         elif choice == 6:
-            num_movies = len(xt.movies_30days)
-            print(f"Found {num_movies} new movies in the past 30 days")
-            if num_movies < 20:
-                for i in range(0,num_movies):
+            NUM_MOVIES = len(xt.movies_30days)
+            print(f"Found {NUM_MOVIES} new movies in the past 30 days")
+            if NUM_MOVIES < 20:
+                for i in range(0,NUM_MOVIES):
                     print(xt.movies_30days[i].title)
 
         elif choice == 7:
-            num_movies = len(xt.movies_7days)
-            print(f"Found {num_movies} new movies in the past 7 days")
-            if num_movies < 20:
-                for i in range(0,num_movies):
+            NUM_MOVIES = len(xt.movies_7days)
+            print(f"Found {NUM_MOVIES} new movies in the past 7 days")
+            if NUM_MOVIES < 20:
+                for i in range(0,NUM_MOVIES):
                     print(xt.movies_7days[i].title)
+
+        elif choice == 8:
+            series_id = input("Series ID: ")
+
+            data = xt._load_series_info_by_id_from_provider(series_id)
+            print(data)
